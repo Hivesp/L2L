843a7269d5acf7aeced29f820fbd00c925bcbabc
Hives Péter <hivesp@gmail.com>
2019-03-08 23:51:35 +0100

    db context factory

--- L2L.data/Model/L2lDbContext.cs
+++ L2L.data/Model/L2lDbContext.cs
@@ -4,10 +4,6 @@ namespace L2L.WeUI.Models
 {
     public class L2lDbContext : DbContext
     {
-        public L2lDbContext(DbContextOptions options) : base(options)
-        {
-        }
-
         public DbSet<Course> Courses { get; set; }
     }
 }
